ec216716d83a11a8da929bd8de4dbf90147c4eb3
Joseph Ferguson <jjferguson1@sheffield.ac.uk>
2025-05-24 00:40:21 +0100

    basic communication with bambu a1

---
+++ python.py
@@ -0,0 +1,47 @@
+# connects printer and sends/recieves messages
+import paho.mqtt.client as mqtt
+import ssl
+import json
+import time
+
+# Printer details
+PRINTER_IP = ''
+ACCESS_CODE = ''
+SERIAL_NUMBER = ''
+REQUEST_TOPIC = f'device/{SERIAL_NUMBER}/request'
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected successfully.")
+        # Subscribe to status reports
+        client.subscribe(f"device/{SERIAL_NUMBER}/report")
+
+
+        payload = {
+                "system": {
+                    "command": "bed_leveling"
+                }
+        }
+
+        # Send request
+        client.publish(REQUEST_TOPIC, json.dumps(payload))
+        print("Command sent.")
+    else:
+        print(f"Connection failed: {rc}")
+
+# Gets printer status
+def on_message(client, userdata, msg):
+    print(f"Message from {msg.topic}: {msg.payload.decode()}")
+
+# Set up client
+client = mqtt.Client()
+client.username_pw_set("bblp", ACCESS_CODE)
+client.tls_set(cert_reqs=ssl.CERT_NONE)
+client.tls_insecure_set(True)
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect and loop
+client.connect(PRINTER_IP, 8883)
+client.loop_forever()
